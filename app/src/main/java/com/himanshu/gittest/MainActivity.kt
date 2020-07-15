732a7a5f74add4f1b0406b2c30755b2cc5aabddc
himanshufoodpanda <himanshu.bahukhandi@foodpanda.com>
2020-07-16 00:33:41 +0530

    master 3.1

--- app/src/main/java/com/himanshu/gittest/MainActivity.kt
+++ app/src/main/java/com/himanshu/gittest/MainActivity.kt
@@ -7,12 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //1.master
-        //2.master
-        //3.master
-        //5.master
-        //6.master
-        //7.master
+        //master 3.1
 
     }
 }
