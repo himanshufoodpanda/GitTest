096bcd2e29a1e4e623cbd2da78f4437a3782fc10
himanshufoodpanda <himanshu.bahukhandi@foodpanda.com>
2020-07-16 08:59:42 +0530

    master 4.2

--- app/src/main/java/com/himanshu/gittest/MainActivity.kt
+++ app/src/main/java/com/himanshu/gittest/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //master 4.1
+        //master 4.2
 
     }
 }
