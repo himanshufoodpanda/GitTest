6584b266626897bacea8d69dd7d174c2519cb0fd
himanshubahukhandi <himanshu.bahukhandi@foodpanda.com>
2022-08-25 00:21:09 +0800

    PIK-1234 : PR is create for awaiting review //PR

--- app/src/main/java/com/himanshu/gittest/Master4File.java
+++ app/src/main/java/com/himanshu/gittest/Master4File.java
@@ -2,6 +2,12 @@ package com.himanshu.gittest;
 
 public class Master4File {
 public void hello(){
-System.out.println("fdjdk");
+//System.out.println("fdjdk");
 }
+
+/*
+*  - name: Show GitHub context
+      env:
+        GITHUB_CONTEXT: ${{ toJson(github) }}
+      run: echo "$GITHUB_CONTEXT"*/
 }
